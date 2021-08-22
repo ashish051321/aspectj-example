0644ad541e6ad8575d68492aab668a3b99bc3157
ashish051321 <ashish.25jl@gmail.com>
2021-08-23 01:52:59 +0530

    typo

--- src/main/java/com/example/aspectjexample/aspects/UserAccessAdvice.java
+++ src/main/java/com/example/aspectjexample/aspects/UserAccessAdvice.java
@@ -11,10 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
-@Configurable
+@Configuration
 @Aspect
 public class UserAccessAdvice {
 
